f91d2a7b64950b3c9b77c3bcd5408da8162570ce
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2024-11-24 15:05:56 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,9 @@ provider "aws" {
 resource "aws_instance" "foo" {
   ami = "ami-0866a3c8686eaeeba"
   instance_type = "t2.micro"
-  region = "us-east-1"
+  tags = {
+    Name = "TF-Instance"
+    }
 }
 
   
